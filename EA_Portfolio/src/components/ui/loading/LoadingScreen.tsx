3d5d135b3833ea76e668a82a598bd5b2b6c2ca9e
PixelOmen <PixelOmen@gmail.com>
2024-08-02 14:51:14 -0700

    Added loading screen animation

--- EA_Portfolio/src/components/ui/loading/LoadingScreen.tsx
+++ EA_Portfolio/src/components/ui/loading/LoadingScreen.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react";
+import styles from "./loadingScreen.module.css";
 
 interface AppLoadingProps {
   className?: string;
@@ -14,16 +15,25 @@ export default function AppLoading( { className = "" }: AppLoadingProps ) {
       setTimeout(() => {
         document.body.style.overflow = "auto";
       }, 50)
-    }, 1000)
+    }, 2000)
   }, []);
 
 
   return (
     <div 
       ref={container}
-      className={`fixed h-screen w-screen top-0 right-0 flex justify-center items-center bg-[#111827] transition-all duration-500 ${className}`}
+      className={`fixed h-screen w-screen top-0 right-0 flex flex-col justify-center items-center bg-[#111827] transition-all duration-500 ${className}`}
     >
-      <div className="">&#x261D; Just a second...</div>
+      <div className="animate-spin border-2 rounded-full border-dotted h-4 w-4"></div>
+      <div className="p-4 flex">
+        Just a second...
+      </div>
+      <div className={`h-5 w-64 border-2 border-black rounded-lg bg-gray-400 ${styles.loadingBarContainer}`}>
+        <div className={`h-full w-0 bg-green-600 rounded-lg ${styles.loadingBar}`}></div>
+      </div>
+      <div className={`${styles.fingerPulse}`}>
+        &nbsp;&nbsp;&#128296;
+      </div>
     </div>
   )
 }
